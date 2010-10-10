4d25a37312d4fb5088f78eacfd45978a7fd80c9c
Jan <polygon@wh2.tu-dresden.de>
2010-10-10 23:46:13 +0200

    Beginning design

---
+++ src/core/deflicker.h
@@ -0,0 +1,16 @@
+#ifndef __DEFLICKER_H
+#define __DEFLICKER_H
+
+#include "CImg.h"
+
+class Deflicker
+{
+  public:
+    Deflicker();
+    Deflicker(cimg_library::CImgList
+
+  private:
+    cimg_library::CImgList<double> images;
+};
+
+#endif // __DEFLICKER_H
